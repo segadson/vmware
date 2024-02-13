7c9e0a408f55a587a8442f889c65438976c97492
Sean Gadson <sean.e.gadson@gmail.com>
2024-02-13 06:57:08 +0000

    updated

--- Code/sddc_manager/sddc_manager_wrapper/__init__.py
+++ Code/sddc_manager/sddc_manager_wrapper/__init__.py
@@ -15,4 +15,4 @@ session = requests.Session()
 session.params = {}
 session.params['api_key'] = SDDC_API_TOKEN
 
-#from .sddc_manager_domain import Domain
+from .sddc_manager_domain import Domain
--- Code/sddc_manager/sddc_manager_wrapper/sddc_manager_domain.py
+++ Code/sddc_manager/sddc_manager_wrapper/sddc_manager_domain.py
@@ -1,8 +1,11 @@
 # sddc_manager_wrapper/sddc_manager_domain.py
+from sddc_manager_global_functions import get_vcf_token
 
+token = get_vcf_token()
 class Domain(object):
-    def __init__(self, id):
+    def __init__(self, id, token):
         self.id = id
+        self.token = token
 
     def info(self):
         return {'id': self.id}
