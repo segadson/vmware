d817dfdea90b67dd55fed238bbe628a16deaebf4
Sean Gadson <sean.e.gadson@gmail.com>
2024-02-22 12:38:27 -0500

    updated for keyerror

---
+++ Code/.history/vcf_automation/error_handling/return_json_20240222122823.py
@@ -0,0 +1,38 @@
+import time
+import json
+import os
+import sys
+import logging
+import requests
+
+
+def return_json(response):
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(response.json())
+        error_message = response.json()['description']
+        logging.error(f'HTTP Error: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    except requests.exceptions.ConnectionError as e:
+        error_message = response.json()['description']
+        logging.error(f'Connection Error: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    except requests.exceptions.Timeout as e:
+        error_message = response.json()['description']
+        logging.error(f'Timeout Error: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    except requests.exceptions.RequestException as e:
+        error_message = response.json()['description']
+        logging.error(f'Request Exception: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    else:
+        return response.json()
---
+++ Code/.history/vcf_automation/error_handling/return_json_20240222123249.py
@@ -0,0 +1,43 @@
+import time
+import json
+import os
+import sys
+import logging
+import requests
+
+
+def return_json(response):
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        if response.json()['description']:
+            error_message = response.json()['description']
+            logging.error(f'HTTP Error: {e}')
+            logging.error(f'Error Message: {error_message}')
+            sys.exit(1)
+        else:
+            error_message = response.json()['value']['error_type']
+            logging.error(f'HTTP Error: {e}')
+            logging.error(f'Error Message: {error_message}')
+            sys.exit(1)
+        return None
+    except requests.exceptions.ConnectionError as e:
+        error_message = response.json()['description']
+        logging.error(f'Connection Error: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    except requests.exceptions.Timeout as e:
+        error_message = response.json()['description']
+        logging.error(f'Timeout Error: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    except requests.exceptions.RequestException as e:
+        error_message = response.json()['description']
+        logging.error(f'Request Exception: {e}')
+        logging.error(f'Error Message: {error_message}')
+        sys.exit(1)
+        return None
+    else:
+        return response.json()
